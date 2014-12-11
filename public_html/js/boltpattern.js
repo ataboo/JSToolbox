d0829a3bdd1418f0f88d379af97f348c0c8954ef
ataboo <bmtoolbox@gmail.com>
2014-12-10 19:47:42 -0700

    Indented Properly

--- public_html/js/boltpattern.js
+++ public_html/js/boltpattern.js
@@ -2,160 +2,160 @@ boltPatternBox = {};
 
 //move to plugins later
 if (typeof String.prototype.startsWith !== 'function') {
-  // see below for better implementation!
-  String.prototype.startsWith = function (str){
-    return this.indexOf(str) === 0;
-  };
+    // see below for better implementation!
+    String.prototype.startsWith = function (str){
+        return this.indexOf(str) === 0;
+    };
 }
 if(typeof String.prototype.repeat !== 'function') {
-	String.prototype.repeat = function (num)
-	{
+    String.prototype.repeat = function (num)
+    {
     	return new Array( num + 1 ).join( this );
-	};
+    };
 }
 
 (function(ctx) {
-	ctx.setupAndStart = function() {
-            ctx.aboutBlurb = "Boilermaker JBox is for info only.\nIf buggy, reset your browser cache.\nComments or Questions: bmtoolbox@gmail.com\n\nLast Updated: December 12, 2014";
+    ctx.setupAndStart = function() {
+        ctx.aboutBlurb = "Boilermaker JBox is for info only.\nIf buggy, reset your browser cache.\nComments or Questions: bmtoolbox@gmail.com\n\nLast Updated: December 12, 2014";
 
-            ctx.studCount = 24;
-            ctx.patternType = 0;
-            ctx.goButton = $("#boltpat-go");
-            ctx.patternSel = $("#boltpat-select");
-            ctx.patternVis = $("#boltpat-vis");
-            ctx.sequenceOut = $("#boltpat-out");
-            ctx.studCountInput = $("#boltpat-stud-count");
+        ctx.studCount = 24;
+        ctx.patternType = 0;
+        ctx.goButton = $("#boltpat-go");
+        ctx.patternSel = $("#boltpat-select");
+        ctx.patternVis = $("#boltpat-vis");
+        ctx.sequenceOut = $("#boltpat-out");
+        ctx.studCountInput = $("#boltpat-stud-count");
 
-            ctx.visuals = [  //s==&nbsp, l==<br>
-                ["s10", "1", "l2", 
-                    "4", "s17", "3", "l2", 
-                    "s10", "2"],
+        ctx.visuals = [  //s==&nbsp, l==<br>
+            ["s10", "1", "l2", 
+                "4", "s17", "3", "l2", 
+                "s10", "2"],
 
-                    ["s10", "1", "l1", 
-                    "s4", "8", "s9", "5", "l1", 
-                    "4", "s17", "3", "l1",
-                    "s4", "6", "s9", "7", "l1",
-                    "s10", "2"]
-            ];
+            ["s10", "1", "l1", 
+                "s4", "8", "s9", "5", "l1", 
+                "4", "s17", "3", "l1",
+                "s4", "6", "s9", "7", "l1",
+                "s10", "2"]
+        ];
 
-            ctx.PATTERN_FOUR = 0;
-            ctx.PATTERN_EIGHT = 1;
+        ctx.PATTERN_FOUR = 0;
+        ctx.PATTERN_EIGHT = 1;
 
-            ctx.setBinds();
-            //ctx.updatePatVis();
-	};
+        ctx.setBinds();
+        //ctx.updatePatVis();
+    };
 	
-	ctx.setBinds = function() {
-		ctx.patternSel.bind('change', function(){
-			//console.log("select changed");
-			ctx.updatePatVis();
-		});
+    ctx.setBinds = function() {
+        ctx.patternSel.bind('change', function(){
+            //console.log("select changed");
+            ctx.updatePatVis();
+        });
 		
-		ctx.goButton.bind('click', function(){
-			//console.log("pushed bootan");
-			ctx.calcPattern();
-		});
+        ctx.goButton.bind('click', function(){
+            //console.log("pushed bootan");
+            ctx.calcPattern();
+        });
                 
-                console.log("got to bind");
-                $("#about-but-bolt").bind('click', function(){
-                    alert(ctx.aboutBlurb);
-                    console.log("tried to alert");
-                });
-	};
+        console.log("got to bind");
+        $("#about-but-bolt").bind('click', function(){
+            alert(ctx.aboutBlurb);
+            console.log("tried to alert");
+        });
+    };
 	
-	ctx.updatePatVis = function() {
-		ctx.patternType = ctx.patternSel[0].selectedIndex;
-		var patStr = ctx.visuals[ctx.patternType];
-		ctx.patternVis.html(ctx.genHtmlVis(patStr));
-	};
+    ctx.updatePatVis = function() {
+        ctx.patternType = ctx.patternSel[0].selectedIndex;
+        var patStr = ctx.visuals[ctx.patternType];
+        ctx.patternVis.html(ctx.genHtmlVis(patStr));
+    };
 	
-	ctx.calcPattern = function() {
-		if(!ctx.studCountValid()) return;
-		ctx.studCount = parseInt(ctx.studCountInput.val());
-		var sequence = "";
-		sequence = ctx.genSequence(ctx.studCount, ctx.patternType);
-		ctx.sequenceOut.html(sequence);
-	};
+    ctx.calcPattern = function() {
+        if(!ctx.studCountValid()) return;
+        ctx.studCount = parseInt(ctx.studCountInput.val());
+        var sequence = "";
+        sequence = ctx.genSequence(ctx.studCount, ctx.patternType);
+        ctx.sequenceOut.html(sequence);
+    };
 	
-	ctx.studCountValid = function() {
-		//TODO
-		var inputParsed = parseInt(ctx.studCountInput.val());
-		if(isNaN(inputParsed)){ 
-			alert("Stud Count is invalid.");
-			return false;
-		}
-		if(inputParsed > 1000 || inputParsed < 8) {
-			alert("Stud Count must be between 8 and 1000.");
-			return false;
-		}
-		if(inputParsed % 2 > 0){
-			alert("Stud Count must be even.");
-			return false;
-		}
-		return true;
-	};
+    ctx.studCountValid = function() {
+        //TODO
+        var inputParsed = parseInt(ctx.studCountInput.val());
+        if(isNaN(inputParsed)){ 
+            alert("Stud Count is invalid.");
+            return false;
+        }
+        if(inputParsed > 1000 || inputParsed < 8) {
+            alert("Stud Count must be between 8 and 1000.");
+            return false;
+        }
+        if(inputParsed % 2 > 0){
+            alert("Stud Count must be even.");
+            return false;
+        }
+        return true;
+    };
 	
-	ctx.genSequence = function(studCount, type){
-		//switch-case if needed later
-		var seqOut = "Pattern: &nbsp";
-		var factor = 0;
-		var basePat = [];
+    ctx.genSequence = function(studCount, type){
+        //switch-case if needed later
+        var seqOut = "Pattern: &nbsp";
+        var factor = 0;
+        var basePat = [];
 		
-		if(type === ctx.PATTERN_FOUR){
-			factor = 4;
-			basePat = [1,3,2,4];
-		} else{
-			factor = 8;
-			basePat = [1,5,3,7,2,6,4,8];
-		}
+        if(type === ctx.PATTERN_FOUR){
+            factor = 4;
+            basePat = [1,3,2,4];
+        } else{
+            factor = 8;
+            basePat = [1,5,3,7,2,6,4,8];
+        }
 		
-		var boldFlag = false;
+        var boldFlag = false;
 		
-		for(var i=0; i < basePat.length; i++){
-			for(var j=basePat[i]; j < studCount + 1; j+=factor){
-				if(j !== 1){
-					seqOut += ", ";
-					if(j === basePat[i]) {
-						seqOut += "&nbsp&nbsp<b>";
-						boldFlag = true;
-					}
-				} else{  //1 still needs bold but no spaces
-					seqOut += "<b>";
-					boldFlag = true;
+        for(var i=0; i < basePat.length; i++){
+            for(var j=basePat[i]; j < studCount + 1; j+=factor){
+                if(j !== 1){
+                    seqOut += ", ";
+                    if(j === basePat[i]) {
+                        seqOut += "&nbsp&nbsp<b>";
+                        boldFlag = true;
+                    }
+                } else{  //1 still needs bold but no spaces
+                    seqOut += "<b>";
+                    boldFlag = true;
 					
-				}
-				seqOut += j;
-				if(boldFlag){
-					seqOut += "</b>";
-				}
-			}
-		}		
-		return seqOut;
-	};
+                }
+                seqOut += j;
+                if(boldFlag){
+                    seqOut += "</b>";
+                }
+            }
+        }		
+        return seqOut;
+    };
 	
-	ctx.genHtmlVis = function(patArr){
-		var outStr = "";
-		for(var i=0; i < patArr.length; i++) {
-			var string = patArr[i];
-			var charCount = 0;
-			if(patArr[i].startsWith('s')) {
-				charCount = parseInt(string.replace('s', ''));
-					outStr = outStr + "&nbsp".repeat(charCount); 			
-			} else {
-				if(patArr[i].startsWith('l')){
-					charCount = parseInt(string.replace('l', ''));
-					outStr = outStr + "<br>".repeat(charCount);
-				} else {
-					outStr = outStr + patArr[i];
-				}
-			}
-		}
-		return outStr;
-	};
+    ctx.genHtmlVis = function(patArr){
+        var outStr = "";
+        for(var i=0; i < patArr.length; i++) {
+            var string = patArr[i];
+            var charCount = 0;
+            if(patArr[i].startsWith('s')) {
+                charCount = parseInt(string.replace('s', ''));
+                outStr = outStr + "&nbsp".repeat(charCount); 			
+            } else {
+                if(patArr[i].startsWith('l')){
+                    charCount = parseInt(string.replace('l', ''));
+                    outStr = outStr + "<br>".repeat(charCount);
+                } else {
+                    outStr = outStr + patArr[i];
+                }
+            }
+        }
+        return outStr;
+    };
 	
 }) (boltPatternBox);
 
 $("#bolt-page").bind('pagecreate', "#bolt-page", function(){
     boltPatternBox.setupAndStart();
-	console.log("ran page init bolt page");
+    console.log("ran page init bolt page");
 });
--- public_html/js/flangetable.js
+++ public_html/js/flangetable.js
@@ -6,177 +6,177 @@ var flangeBox = {};  //Used for context to coordinate between the functions.
  * alert box.  It was designed using Jquery Mobile and Cordova.*/
 (function(context) {	
 
-	context.startLoad = function() {
-		$(function() {
+    context.startLoad = function() {
+        $(function() {
             //FastClick.attach(document.body);
         });
-		context.aboutBlurb = "Boilermaker JBox is for info only.\nIf buggy, reset your browser cache.\nComments or Questions: bmtoolbox@gmail.com\n\nLast Updated: December 12, 2014";
+        context.aboutBlurb = "Boilermaker JBox is for info only.\nIf buggy, reset your browser cache.\nComments or Questions: bmtoolbox@gmail.com\n\nLast Updated: December 12, 2014";
                 
-		$("#flange-send").click(function(){
-			context.calcFlange();
-		});
+        $("#flange-send").click(function(){
+            context.calcFlange();
+        });
 	
-		$("#stud-send").click(function(){
-			context.calcStud();
-		});
+        $("#stud-send").click(function(){
+            context.calcStud();
+        });
 		
-		context.flangeSelClass = $(".flange-sel");
+        context.flangeSelClass = $(".flange-sel");
 		
-		context.sizeSel = $("#select-flange-size");
-		context.rateSel = $("#select-flange-rating");
-		context.studSel = $("#stud-select");
+        context.sizeSel = $("#select-flange-size");
+        context.rateSel = $("#select-flange-rating");
+        context.studSel = $("#stud-select");
 		
 		
-		$.getJSON("data/FlangeArrays.json").done(function(data){
-			context.postLoad(data);
-		});
-	};
+        $.getJSON("data/FlangeArrays.json").done(function(data){
+            context.postLoad(data);
+        });
+    };
 	
-	//after the JSON is done loading this is called to run the rest of the setup.	
-	context.postLoad = function(data) {
-		context.data = data;
-		context.popSelects();
-		context.addSelectChange();
+    //after the JSON is done loading this is called to run the rest of the setup.	
+    context.postLoad = function(data) {
+        context.data = data;
+        context.popSelects();
+        context.addSelectChange();
 		
-	};
+    };
 	
-	context.popSelects = function() {
-		var data = context.data;
-		var selectSize = context.sizeSel;
-		var selectRate = context.rateSel;
-		var selectStud = context.studSel;
+    context.popSelects = function() {
+        var data = context.data;
+        var selectSize = context.sizeSel;
+        var selectRate = context.rateSel;
+        var selectStud = context.studSel;
 		
-		$.each(data.fSizes, function(i, value) {
-	   		selectSize.append($('<option>').text(value + "\"").attr('value', value));
-		});
-		$.each(data.fRatings, function(i, value) {
-	   		selectRate.append($('<option>').text(value + "#").attr('value', value));
-		});
+        $.each(data.fSizes, function(i, value) {
+            selectSize.append($('<option>').text(value + "\"").attr('value', value));
+        });
+        $.each(data.fRatings, function(i, value) {
+            selectRate.append($('<option>').text(value + "#").attr('value', value));
+        });
 	
-		var studSizes = data.studSizes;
-		var studSizeOrd = data.studSizeOrdered;
-		var studStatsOrdered = [];
+        var studSizes = data.studSizes;
+        var studSizeOrd = data.studSizeOrdered;
+        var studStatsOrdered = [];
 		
-		for(var i = 0; i < studSizeOrd.length; i++) {
-			selectStud.append($('<option>').text(studSizeOrd[i] + "\"").attr('value', studSizeOrd[i] + "\""));
-			var studSizeOrdString = studSizeOrd[i].toString();
-			//console.log(studSizeOrdString);
-			studStatsOrdered[i] = [studSizes[studSizeOrdString]];
-		} 
+        for(var i = 0; i < studSizeOrd.length; i++) {
+            selectStud.append($('<option>').text(studSizeOrd[i] + "\"").attr('value', studSizeOrd[i] + "\""));
+            var studSizeOrdString = studSizeOrd[i].toString();
+            //console.log(studSizeOrdString);
+            studStatsOrdered[i] = [studSizes[studSizeOrdString]];
+        } 
 		
-		context.studStatsOrdered = studStatsOrdered;
-		context.sendBut = $("#flange-send");
-		context.sendBut.prop('disabled', true);
+        context.studStatsOrdered = studStatsOrdered;
+        context.sendBut = $("#flange-send");
+        context.sendBut.prop('disabled', true);
 		
-		context.studBut = $("#stud-send");
-                context.studBut.prop('disabled', true);
+        context.studBut = $("#stud-send");
+        context.studBut.prop('disabled', true);
                 
-                $("#about-but-flange").bind('click', function(){
-                    alert(context.aboutBlurb);
-                });
+        $("#about-but-flange").bind('click', function(){
+            alert(context.aboutBlurb);
+        });
                 
-		context.addSelectChange();	
-	};
+        context.addSelectChange();	
+    };
 	
-	context.addSelectChange = function() {
-		context.flangeSelClass.bind('change', function(){
-			//console.log("change");
-			context.updateSendButton();
-		});
-	};
+    context.addSelectChange = function() {
+        context.flangeSelClass.bind('change', function(){
+            //console.log("change");
+            context.updateSendButton();
+        });
+    };
 	
-	context.updateSendButton = function () {
-		/*
+    context.updateSendButton = function () {
+        /*
 		if(typeof(context.rateSel[0].selectedIndex) === 'undefined' ||
 		typeof(context,studSel[0].selectedIndex) === 'undefined') { 
 			console.log("shit is all undefined");
 			return;
 		}
-		*/
+         */
 		
-                context.sendBut.prop('disabled', context.rateSel[0].selectedIndex == 0 || context.sizeSel[0].selectedIndex == 0)
-		//context.sendBut.button("refresh");
+        context.sendBut.prop('disabled', context.rateSel[0].selectedIndex == 0 || context.sizeSel[0].selectedIndex == 0)
+        //context.sendBut.button("refresh");
 		
-                context.studBut.prop('disabled', context.studSel[0].selectedIndex == 0);
-               // console.log(context.studSel[0].selectedIndex);
-		//context.studBut.button("refresh");
-	};
+        context.studBut.prop('disabled', context.studSel[0].selectedIndex == 0);
+        // console.log(context.studSel[0].selectedIndex);
+        //context.studBut.button("refresh");
+    };
 	
-	context.calcStud = function () {  //called by stud only button 
-		context.studSize = context.studSel.val().replace('"', '');
-		context.studStats = context.getStudStats(context.studSize);
-		//console.log("studSize is: " + context.studSize + ", studStats are: " + context.studStats); 
-		context.displayStud(context.studStats);
-	};
+    context.calcStud = function () {  //called by stud only button 
+        context.studSize = context.studSel.val().replace('"', '');
+        context.studStats = context.getStudStats(context.studSize);
+        //console.log("studSize is: " + context.studSize + ", studStats are: " + context.studStats); 
+        context.displayStud(context.studStats);
+    };
 	
-	context.displayStud = function(studStats){
-		var studSize = context.studSize;
-		var studString = ("Stud Size: " + studSize + "\"");
-		var toolString = ("Wrench: " + studStats[0] + "\",   Drift Pin: " + studStats[1] + "\"");
-		var torqueString = ("B7 Torque: " + studStats[3] + " ft-lbs \nB7M Torque: " + studStats[2] + " ft-lbs");
+    context.displayStud = function(studStats){
+        var studSize = context.studSize;
+        var studString = ("Stud Size: " + studSize + "\"");
+        var toolString = ("Wrench: " + studStats[0] + "\",   Drift Pin: " + studStats[1] + "\"");
+        var torqueString = ("B7 Torque: " + studStats[3] + " ft-lbs \nB7M Torque: " + studStats[2] + " ft-lbs");
 		
-		var outString = (studString + "\n" +
-			toolString + "\n" +
-			torqueString);
+        var outString = (studString + "\n" +
+                toolString + "\n" +
+                torqueString);
 		
-		alert(outString);
-	};
+        alert(outString);
+    };
 	
-	context.calcFlange = function() {
-		var sSize = context.sizeSel;
-		var sRate = context.rateSel;
+    context.calcFlange = function() {
+        var sSize = context.sizeSel;
+        var sRate = context.rateSel;
 	
-		var rateInd = sRate[0].selectedIndex;  //flange rating select index
-		context.rateVal = sRate.val();
-		context.sizeVal = $(sSize).val();  //might not make global
+        var rateInd = sRate[0].selectedIndex;  //flange rating select index
+        context.rateVal = sRate.val();
+        context.sizeVal = $(sSize).val();  //might not make global
 		
-		//Inside the $.each() callback above, you would do $.each(this.subaction, function() { alert(this.name); });, that would give you A, B, C, etc. - test it out here:
+        //Inside the $.each() callback above, you would do $.each(this.subaction, function() { alert(this.name); });, that would give you A, B, C, etc. - test it out here:
 			
-		context.flangeStats = context.getFStats(rateInd, context.sizeVal);
-		//console.log("flange stats is defined and [0] is: " + context.flangeStats[0]);
-		context.studStats = context.getStudStats(context.flangeStats[0]);
-		//console.log("studStats is: " + context.studStats);
-		context.displayFlange(context.flangeStats, context.studStats);
-	};
+        context.flangeStats = context.getFStats(rateInd, context.sizeVal);
+        //console.log("flange stats is defined and [0] is: " + context.flangeStats[0]);
+        context.studStats = context.getStudStats(context.flangeStats[0]);
+        //console.log("studStats is: " + context.studStats);
+        context.displayFlange(context.flangeStats, context.studStats);
+    };
 	
-	context.getFStats = function (rateIndex, sizeVal){
-		var fStatArr = [
-			0,  //First index in <select> is "rate" label
-			context.data.fStats150,
-			context.data.fStats300,
-			context.data.fStats400,
-			context.data.fStats600,
-			context.data.fStats900,
-			context.data.fStats1500
-		];
+    context.getFStats = function (rateIndex, sizeVal){
+        var fStatArr = [
+            0,  //First index in <select> is "rate" label
+            context.data.fStats150,
+            context.data.fStats300,
+            context.data.fStats400,
+            context.data.fStats600,
+            context.data.fStats900,
+            context.data.fStats1500
+        ];
 		
-		var fStats = fStatArr[rateIndex];
-		var statString = [];
-		$.each(fStats, function(i, value){
-			//console.log("checked: " + i + " against: " + sizeVal);
-			if(i == sizeVal) {
-				//console.log("getFstats returned: " + value);
-				statString = value;
-			}
-		});
-		return statString;
-	};
+        var fStats = fStatArr[rateIndex];
+        var statString = [];
+        $.each(fStats, function(i, value){
+            //console.log("checked: " + i + " against: " + sizeVal);
+            if(i == sizeVal) {
+                //console.log("getFstats returned: " + value);
+                statString = value;
+            }
+        });
+        return statString;
+    };
 	
-	context.getStudStats = function (studSize){
-		var retVal = [];
-		$.each(context.data.studSizes, function(i, value) {
-			if(i === studSize) {
-				//console.log("got stud: " + value);
-				retVal = value;
-			}
-			//console.log("Checking: " + studSize + " against: " + i + " getting: " + (studSize == i));
-		});
+    context.getStudStats = function (studSize){
+        var retVal = [];
+        $.each(context.data.studSizes, function(i, value) {
+            if(i === studSize) {
+                //console.log("got stud: " + value);
+                retVal = value;
+            }
+            //console.log("Checking: " + studSize + " against: " + i + " getting: " + (studSize == i));
+        });
 		
-		return retVal;
-		//console.log("Should still be: " + context.torqueStats);
-	};
+        return retVal;
+        //console.log("Should still be: " + context.torqueStats);
+    };
 	
-	context.displayFlange = function(flangeStats, studStats) {
+    context.displayFlange = function(flangeStats, studStats) {
 	//studStats = studSize: [wrench size, drift pin, b7m torque, b7 torque]
 	//flangeStats = flangeSize: [studSize, studIndex, studCount, studLength] 
 	
@@ -189,7 +189,7 @@ var flangeBox = {};  //Used for context to coordinate between the functions.
 		studString + "\n"
 		+ toolString + "\n"
 		+ torqueString);
-	};
+    };
 	
 })(flangeBox);
 
